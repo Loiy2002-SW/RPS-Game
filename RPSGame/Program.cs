42c2cf12a0996aa55a35921c59b2706e7acdbe76
loiy <lloiy9310@gmail.com>
2024-06-24 13:22:45 +0300

    Finish the game functionalities

--- RPSGame/Program.cs
+++ RPSGame/Program.cs
@@ -1,13 +1,42 @@
-﻿namespace RPSGame
+﻿
+namespace RPSGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Player p = new Player("Loai");
+            Console.WriteLine("Welcome to Rock, Paper, Scissors!");
 
-            Console.WriteLine(p.ChooseMove());
+            Console.Write("Enter your name: ");
+
+            string playerName = askTheUserForHisName();
+
+           
+           
+
+            RPSGame game = new RPSGame(playerName);
+            game.PlayGame();
+
+            Console.WriteLine("Thanks for playing!");
+
+        }
+
+        private static string askTheUserForHisName()
+        {
+
+            string name = "";
+            while (true)
+            {
+
+                name = Console.ReadLine();
+
+                if (name.Length < 2)
+                    Console.WriteLine("Please enter a valid name");
+                else
+                    return name;
+
+            }
 
         }
     }
