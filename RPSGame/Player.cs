a4ae94282d2421cc5b9fe4eab715f607c324c629
loiy <lloiy9310@gmail.com>
2024-06-24 12:22:34 +0300

    ADD player class

---
+++ RPSGame/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSGame
+{
+    internal class Player
+    {
+        string name;
+        int score;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.score = 0;
+        }
+
+
+        public string ChooseMove() {
+
+
+            Console.WriteLine("Choose a move:\n 1: Rock\n 2: Paper\n 3: Scissors");
+
+            while (true)
+            {
+                string reslut = Console.ReadLine();
+
+                if (reslut == "1" || reslut == "2" || reslut == "3")
+                    return reslut;
+
+                else
+                    Console.WriteLine("Invalid input, please enter a correct move:");
+            }
+
+        }
+    }
+}
